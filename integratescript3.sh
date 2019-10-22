fff9dcf6642fb106af708e6debc6b62803e1408a
Jussi Seppälä <jussi.k.seppala@gmail.com>
2019-10-22 13:05:57 +0300

    add

---
+++ integratescript3.sh
@@ -0,0 +1,28 @@
+echo Script 3
+echo Read value from AWS Parameter Store
+SSM_PARAM_NAME=PROC_REPORTING_CALL
+SSM_VALUE=`aws ssm get-parameters --with-decryption --names "${SSM_PARAM_NAME}" --region eu-west-1 --query 'Parameters[*].Value' --output text`
+echo Parameter read successfully
+
+find="ENTITY_TYPE"
+replace="FISCAL_YEAR"
+result=${SSM_VALUE//$find/$replace}
+echo $replace
+curl -d "$result" -H "Content-Type: application/json" -X GET http://localhost:8080/integration/integrate
+
+replace="LEDGER_ACCOUNT"
+result=${SSM_VALUE//$find/$replace}
+echo $replace
+curl -d "$result" -H "Content-Type: application/json" -X GET http://localhost:8080/integration/integrate
+
+replace="DIMENSION"
+result=${SSM_VALUE//$find/$replace}
+echo $replace
+curl -d "$result" -H "Content-Type: application/json" -X GET http://localhost:8080/integration/integrate
+
+replace="BUSINESS_PARTNER"
+result=${SSM_VALUE//$find/$replace}
+echo $replace
+curl -d "$result" -H "Content-Type: application/json" -X GET http://localhost:8080/integration/integrate
+
+echo Script completed!
